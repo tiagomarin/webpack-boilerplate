79aecccfd727f4433cfadc735a078351c6c93daa
Tiago Marin <tiago.lelinski@gmail.com>
2022-07-12 15:27:26 -0300

    attempt to fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,19 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+     {
+       test: /\.(png|svg|jpg|jpeg|gif)$/i,
+       type: 'asset/resource',
+     },
+    ],
   },
 };
